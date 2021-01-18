ae06ed775356084109807fe36048c03bc6e9d98c
esujeewa <sujeewa@millenniumitesp.com>
2021-01-18 21:03:19 +0530

    precenters item fixed

--- Intempio.Meetings.Home/ClientApp/src/components/login.js
+++ Intempio.Meetings.Home/ClientApp/src/components/login.js
@@ -3,11 +3,18 @@ import history from './history';
 import Events from './events';
 import moment from 'moment';
 import { ActivityLog } from './ActivityLog';
+//import ReCAPTCHA from 'react-google-recaptcha';
+
+
+//const recaptchaRef = React.createRef();
+//const SITE_KEY = "6LehCi8aAAAAACMZorVoHGEZpFdUHNqyKSSkz6tV";
 export class Login extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { firstName: '', lastName: '', email: '', loading: false, emailinput: '', inputFirstName: '', inputLastName: '', status: '', colour: '#FFFFFF', unrecognizedLogin: false, excelLogin: false ,sqllogin:false };
+        this.state = { firstName: '', lastName: '', email: '', loading: false, emailinput: '', inputFirstName: '', inputLastName: '', status: '', colour: '#FFFFFF', unrecognizedLogin: false, excelLogin: false, sqllogin: false };
+
+        this.reCaptchaRef = React.createRef();
     }
 
     async getSettings() {
@@ -21,7 +28,7 @@ export class Login extends Component {
         const finalresult = await response.json().then(async (resonse) => {
             this.setState({ loading: false });
             var item = JSON.parse(resonse.value);
-            this.setState({ loading: false, unrecognizedLogin: item.value[0].fields.UnrecognizedLogin, excelLogin: item.value[0].fields.Excellogin, sqllogin: item.value[0].fields.SQLlogin});
+            this.setState({ loading: false, unrecognizedLogin: item.value[0].fields.UnrecognizedLogin, excelLogin: item.value[0].fields.Excellogin, sqllogin: item.value[0].fields.SQLlogin });
             if (item && item.value[0].fields.Colour) {
                 document
                     .documentElement.style.setProperty("--color-surface", item.value[0].fields.Colour);
@@ -115,9 +122,9 @@ export class Login extends Component {
                 var userObj = { firstName: items[0].fields.FirstName, lastName: items[0].fields.LastName, email: items[0].fields.Email, inputFirstName: this.state.inputFirstName, inputLastName: this.state.inputLastName, exp: moment().add(7, 'days'), unrecognizedLogin: false };
                 this.setState({ firstName: items[0].fields.FirstName, lastName: items[0].fields.LastName, email: items[0].fields.Email });
 
-              
 
-                if (this.state.email && this.state.email != '' ) {
+
+                if (this.state.email && this.state.email != '') {
                     localStorage.setItem("userToken", JSON.stringify(userObj));
                     history.push('/');
                 }
@@ -142,7 +149,7 @@ export class Login extends Component {
             headers: { 'Content-Type': 'application/json' }
 
         });
-       
+
         await response.json().then((resonse) => {
             this.setState({ loading: false });
             var items = JSON.parse(resonse.value).values;
@@ -173,8 +180,9 @@ export class Login extends Component {
 
     async getSQLUserUserInfo() {
         this.setState({ loading: true });
-        //const query = new URLSearchParams(this.props.location.search);
-        //const eventID = query.get('eventid')
+
+    
+
         const response = await fetch('Meeting/GetSQLUserByEmail?email=' + this.state.emailinput, {
             method: "GET",
             headers: { 'Content-Type': 'application/json' }
@@ -186,7 +194,7 @@ export class Login extends Component {
             var items = resonse;
 
 
-            if (items.length>0) {
+            if (items.length > 0) {
                 var userObj = { firstName: null, lastName: null, email: this.state.emailinput, inputFirstName: this.state.inputFirstName, inputLastName: this.state.inputLastName, exp: moment().add(7, 'days'), unrecognizedLogin: false };
                 this.setState({ firstName: null, lastName: null, email: this.state.emailinput });
 
@@ -197,8 +205,8 @@ export class Login extends Component {
                 }
             }
             else {
-                    this.setState({ status: 2 });
-                
+                this.setState({ status: 2 });
+
             }
         }).catch((error) => {
             this.setState({ status: 2 });
@@ -206,7 +214,7 @@ export class Login extends Component {
         });
     }
     doLogin = () => {
-        ActivityLog.getStringValue(this.state.emailinput, "login","In progress");
+        ActivityLog.getStringValue(this.state.emailinput, "login", "In progress");
         if (this.validateInput()) {
             if (this.state.unrecognizedLogin == true) {
                 var userObj = { firstName: null, lastName: null, email: this.state.emailinput, inputFirstName: this.state.inputFirstName, inputLastName: this.state.inputLastName, exp: moment().add(7, 'days'), unrecognizedLogin: true };
@@ -237,6 +245,8 @@ export class Login extends Component {
 
     }
 
+    reRef = () => React.createRef();
+
     onchangeEmail(e) {
         this.setState({ status: 0 });
         this.setState({ emailinput: e.target.value });
@@ -258,27 +268,34 @@ export class Login extends Component {
     validateInput() {
 
 
+ 
 
+   
         if (!this.validateEmail(this.state.emailinput)) {
             this.setState({ status: 1 });
             return false;
         }
 
-        if (this.state.inputFirstName==='') {
+        if (this.state.inputFirstName === '') {
             this.setState({ status: 4 });
             return false;
         }
 
-        if (this.state.inputLastName==='') {
-            this.setState({ status: 5});
+        if (this.state.inputLastName === '') {
+            this.setState({ status: 5 });
             return false;
         }
         return true;
     }
-    
+
     componentDidMount() {
         this.getSettingsv2();
     }
+
+
+
+
+
 
     render() {
         return (
@@ -297,6 +314,9 @@ export class Login extends Component {
                                     placeholder="* FirstName" value={this.state.inputFirstName} onChange={this.onchangeFname.bind(this)} />
                                 <input id='footer-lname' type="text"
                                     placeholder="* LastName" value={this.state.inputLastName} onChange={this.onchangeLname.bind(this)} />
+                   
+                               
+                         
                                 <button onClick={this.doLogin} className="loginButton"> Login</button>
                                 {(this.state.status === 1) && < div className="info-message"> Please enter valid email </div>}
                                 {(this.state.status === 2) && < div className="info-message"> This email is not registered for this event </div>}
@@ -305,7 +325,11 @@ export class Login extends Component {
                                 {(this.state.status === 5) && < div className="info-message"> Please enter your last name  </div>}
                                 {(this.state.loading) && < div className="info-message"> Please wait...   </div>}
                             </div>
+
+                           
                         </div>
+
+                        
                     </main>
                 </div>
                 <footer id="footer">
